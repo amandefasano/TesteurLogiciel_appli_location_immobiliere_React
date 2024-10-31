2d3ea1cd2d37436d32c19a3e2064ccf1cad77815
Amandine Douguedroit <amandine.douguedroit@gmail.com>
2024-10-31 14:15:21 +0200

    Styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,30 @@
+import styled from "styled-components";
+
 export default function Footer() {
+  const Footer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: #ffffff;
+    background-color: #000000;
+    width: 100%;
+    position: absolute;
+    left: 0;
+  `;
+
+  const StyledP = styled.p`
+    margin: 1.812rem 0; /*29px 0*/
+    font-size: 1.5rem; /*24px*/
+  `;
+
+  const Logo = styled.img`
+    margin-top: 4.125rem; /*66px*/
+  `
+
   return (
-    <>
-      <img src="/src/assets/LOGO-white.svg"></img>
-      <p>© 2020 Kasa. All rights reserved</p>
-    </>
+    <Footer>
+      <Logo src="/src/assets/LOGO-white.svg"></Logo>
+      <StyledP>© 2020 Kasa. All rights reserved</StyledP>
+    </Footer>
   );
 }
