6d4a5542c1fffead542c8953cc6a2ac83c9be727
Amandine Douguedroit <amandine.douguedroit@gmail.com>
2024-10-31 18:19:31 +0200

    Fix banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,42 +1,44 @@
 import styled from "styled-components";
 
 export default function Banner({ img_src, p }) {
+  const Wrapper = styled.div`
+    position: relative;
+    max-width: 77.5rem; /*1240px*/
+    height: 13.937rem; /*223px*/
+  `;
+
   const Image = styled.img`
     width: 100%;
     height: 13.937rem; /*223px*/
-    border-radius: 1.562rem; /*25px*/
+    border-radius: 25px;
     box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
     object-fit: cover;
   `;
 
-  const WrapP = styled.div`
+  const StyledP = styled.p`
+    position: absolute;
+    top: 0;
+    left: 0;
     display: flex;
     align-items: center;
     justify-content: center;
     background-color: #000000;
     opacity: 0.6;
-    width: 77.5rem; /*1240px*/
-    // width: 67.562rem; /*1081px*/
-    height: 13.937rem; /*223px*/
-    border-radius: 1.562rem; /*25px*/
-    position: absolute;
-    left: 6.312rem; /*101px*/
-    top: 10.25rem; /*164px*/
-  `;
-
-  const StyledP = styled.p`
+    width: 100%;
+    height: 100%;
     color: #ffffff;
-    opacity: 100%;
     font-size: 3rem; /*48px*/
     font-weight: bold;
+    margin: 0;
+    border-radius: 25px;
   `;
 
   return (
     <>
-      <Image src={img_src}></Image>
-      <WrapP>
+      <Wrapper id="banner">
+        <Image src={img_src}></Image>
         <StyledP className={p ? "banner-text" : "display-none"}>{p}</StyledP>
-      </WrapP>
+      </Wrapper>
     </>
   );
 }
